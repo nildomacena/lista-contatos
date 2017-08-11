1f5b72c668c6f0d24b8240559d0af2f73df9e802
Nildo <nildo.777@gmail.com>
2017-08-11 14:01:46 -0300

    Adicionado plugin do facebook

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,3 +1,4 @@
+import { Fire } from './../../providers/fire';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
@@ -15,11 +16,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public fire: Fire
+    ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
 
+  loginComFacebook(){
+    
+  }
 }
--- src/providers/fire.ts
+++ src/providers/fire.ts
@@ -4,14 +4,23 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
 import * as firebase from 'firebase';
 
 @Injectable()
 export class Fire {
 
-  constructor(public sim: Sim, public alertCtrl: AlertController) {
+  constructor(public sim: Sim, public alertCtrl: AlertController, public auth: AngularFireAuth) {
     console.log('Hello FireProvider Provider');
   }
+
+
+    loginComFacebook(){
+      
+      firebase.auth().
+    }
+
 
     login(){
       this.sim.requestReadPermission()
